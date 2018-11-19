739ecd68eb8c5ca59bd3ab61e34cb20fc287f6bb
Andrea <andry25.9.99@gmail.com>
2018-11-19 15:29:55 +0100

    Added idle parser

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -17,6 +17,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Windows.Interop;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
 
 namespace WpfApp2
 {
@@ -27,25 +30,81 @@ namespace WpfApp2
     {
         //Colors from https://flatuicolors.com/palette/de
         SolidColorBrush GreenBackground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#26de81"));
+        SolidColorBrush BlueBackground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#3867d6")); //Blue when user is in pause
         SolidColorBrush RedBackground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ff4757"));
         SolidColorBrush OrangeBackground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#fa8231"));
 
-        string defaultTimeString = "01:00:00";
+        const string defaultTimeString = "01:00:00";
+        const string defaultPauseString = "00:15:00";
+
+        TimeSpan defaultPauseTimeSpan = TimeSpan.Parse(defaultPauseString);
+
         int[] backgroundPopup = new int[] {5};
-        
-        
+
+        #region Get Cursor Posion func
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+        public static Point GetMousePosition()
+        {
+            Win32Point w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+            return new Point(w32Mouse.X, w32Mouse.Y);
+        }
+        #endregion
+
+        private Point LatestMousePosition;
+
         //Main timer Init
         System.Windows.Threading.DispatcherTimer mainTimer = new System.Windows.Threading.DispatcherTimer();
 
-        private TimeSpan CurrestTimespan;
+        private TimeSpan CurrentTimespan;
+        private TimeSpan SettedTimespan;
+
+        private TimeSpan CurrentMouseIdleTime = new TimeSpan(0,0,0);
+        private bool IsIdle = false;
+        
 
         private void mainTimer_Tick(object sender, EventArgs e)
         {
-            CurrestTimespan -= new TimeSpan(0, 0, 1);
-            ClockTxt.Text = CurrestTimespan.ToString(@"hh\:mm\:ss");
-            if (CurrestTimespan.TotalSeconds <= 0)
+            CurrentTimespan -= new TimeSpan(0, 0, 1);
+            ClockTxt.Text = CurrentTimespan.ToString(@"hh\:mm\:ss");
+            var newMousePosition = GetMousePosition();
+            if (LatestMousePosition== newMousePosition)
             {
-                if (CurrestTimespan.TotalSeconds == 0  || backgroundPopup.Any(x => CurrestTimespan.TotalSeconds % x == 0))
+                CurrentMouseIdleTime += new TimeSpan(0,0,1);
+            }
+            else
+            {
+                LatestMousePosition = newMousePosition;
+                CurrentMouseIdleTime = new TimeSpan(0, 0, 0);
+                if (IsIdle)
+                {
+                    //user is back, was in idle
+                    //Restart timer
+                    CurrentTimespan = SettedTimespan;
+                    HomeWindow.Background = GreenBackground;
+                }
+                IsIdle = false;
+            }
+
+            if (CurrentMouseIdleTime > defaultPauseTimeSpan)
+            {
+                //It seems like that's a pause
+                IsIdle = true;
+                HomeWindow.Background = BlueBackground;
+            }
+
+            if (!IsIdle && CurrentTimespan.TotalSeconds <= 0)
+            {
+                if (CurrentTimespan.TotalSeconds == 0  || backgroundPopup.Any(x => CurrentTimespan.TotalSeconds % x == 0))
                 {
                     this.Topmost = true;
                     this.Activate();
@@ -67,9 +126,14 @@ namespace WpfApp2
             MainBtn.Content = "START";
             mainTimer.Tick += new EventHandler(mainTimer_Tick);
 
+            LatestMousePosition = GetMousePosition();
+
+            ClockTxt.Text = defaultTimeString;
         }
 
-        
+       
+
+
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -85,6 +149,7 @@ namespace WpfApp2
 
         private void OnMainBtnClick(object sender, RoutedEventArgs e)
         {
+            LatestMousePosition = GetMousePosition();
             if (mainTimer.IsEnabled)
             {
 
@@ -129,7 +194,8 @@ namespace WpfApp2
                 ClockTxt.Text = ts.ToString(@"hh\:mm\:ss");
                 mainTimer.Interval = new TimeSpan(0, 0, 1);
 
-                CurrestTimespan = ts;
+                SettedTimespan = ts;
+                CurrentTimespan = ts;
                 mainTimer.Start();
 
 
@@ -139,6 +205,15 @@ namespace WpfApp2
 
                 //MessageBox.Show("YA BETTER SHTAP!");
             }
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            this.Topmost = true;
+            this.Activate();
+            this.BringIntoView();
+            this.Focus();
+            this.Topmost = false;
         }
     }
 }
