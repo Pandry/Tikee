b1ece75ea5b71fadb9779de76c765ad5a8c7dabb
Andrea <andry25.9.99@gmail.com>
2018-11-19 02:39:34 +0100

    First working timer

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Interop;
 
 namespace WpfApp2
 {
@@ -20,13 +25,51 @@ namespace WpfApp2
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Colors from https://flatuicolors.com/palette/de
+        SolidColorBrush GreenBackground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#26de81"));
+        SolidColorBrush RedBackground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ff4757"));
+        SolidColorBrush OrangeBackground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#fa8231"));
+
+        string defaultTimeString = "01:00:00";
+        int[] backgroundPopup = new int[] {1,5};
+        
+        
+        //Main timer Init
+        System.Windows.Threading.DispatcherTimer mainTimer = new System.Windows.Threading.DispatcherTimer();
+
+        private TimeSpan CurrestTimespan;
+
+        private void mainTimer_Tick(object sender, EventArgs e)
+        {
+            CurrestTimespan -= new TimeSpan(0, 0, 1);
+            ClockTxt.Text = CurrestTimespan.ToString(@"hh\:mm\:ss");
+            if (CurrestTimespan.TotalSeconds < 0)
+            {
+                if (backgroundPopup.Any(x => CurrestTimespan.TotalSeconds % x == 0))
+                {
+                    this.Activate();
+                }
+                HomeWindow.Background = RedBackground;
+            }
+        }
+
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+            HomeWindow.Background = GreenBackground;
+            MainBtn.Content = "START";
+            mainTimer.Tick += new EventHandler(mainTimer_Tick);
+
         }
+
+        
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            Keyboard.ClearFocus();
             if (e.ChangedButton == MouseButton.Left)
                 this.DragMove();
         }
@@ -36,12 +79,61 @@ namespace WpfApp2
             this.Close();
         }
 
-        
-        private void OnStopBtnClick(object sender, RoutedEventArgs e)
+        private void OnMainBtnClick(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("YA BETTER SHTAP!");
-            
+            if (mainTimer.IsEnabled)
+            {
+
+                mainTimer.Stop();
+                ClockTxt.Text = defaultTimeString;
+                MainBtn.Content = "START";
+                HomeWindow.Background = OrangeBackground;
+            }
+            else
+            {
+                TimeSpan ts;
+                try
+                {
+                    ts = TimeSpan.Parse(ClockTxt.Text);
+                }
+                catch (ArgumentNullException ex)
+                {
+                    MessageBox.Show("String empty! Blah blah");
+                    ClockTxt.Text = defaultTimeString;
+                    return;
+                }
+                catch (FormatException ex)
+                {
+                    MessageBox.Show("The format isn't fine! Blah blah");
+
+                    ClockTxt.Text = defaultTimeString;
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Something bad happened...");
+                    return;
+                }
+
+                if (ts > new TimeSpan(23, 59, 59))
+                {
+                    MessageBox.Show("Something bad happened...");
+                    return;
+                }
+
+                ClockTxt.Text = ts.ToString(@"hh\:mm\:ss");
+                mainTimer.Interval = new TimeSpan(0, 0, 1);
+
+                CurrestTimespan = ts;
+                mainTimer.Start();
+
+
+
+                MainBtn.Content = "STOP";
+                HomeWindow.Background = GreenBackground;
+
+                //MessageBox.Show("YA BETTER SHTAP!");
+            }
         }
-        
     }
 }
